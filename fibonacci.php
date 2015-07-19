874c1471546a021a9bb8b0666066d61d22b9f356
ranjeet <amitlranjeet_5619@yahoo.co.in>
2015-07-19 01:32:30 -0800

    new febonnasi series example

---
+++ fibonacci.php
@@ -0,0 +1,35 @@
+<?php
+/****** Example of Fabonassi series
+
+Steps  Values
+
+0 		0
+1       1
+2 		1	
+3 		2
+4 		3
+5 		5
+6 		8
+7 		13
+8 		21
+9 		34
+*****/
+
+function calculateFibonacci( $step ) {
+	
+	$arrintValue[0] = 0;
+	$arrintValue[1] = 1;
+	
+	for ( $i=2; $i <= $step; $i++ ) {
+		$arrintValue[$i] 	= $arrintValue[$i - 2] + $arrintValue[$i - 1];		
+	}	
+
+	return $arrintValue;
+}
+
+$step = 6;
+$result = calculateFibonacci( $step )[$step];
+
+echo 'Result => ' . $result;
+
+?>
